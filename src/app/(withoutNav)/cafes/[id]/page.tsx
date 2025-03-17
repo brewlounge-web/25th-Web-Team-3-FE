bcbffbd8fdbbde2752820a665990fb41daccc6d7
brewlounge-web <brewlounge3@gmail.com>
2025-03-17 15:31:04 +0900

    ssg 적용 전으로 변경

--- src/app/(withoutNav)/cafes/[id]/page.tsx
+++ src/app/(withoutNav)/cafes/[id]/page.tsx
@@ -29,7 +29,7 @@ import {
   toggleInput,
   toggleLabel,
 } from './page.css';
-
+//ssg 적용 전
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const detailPageId = (await params).id;
 
